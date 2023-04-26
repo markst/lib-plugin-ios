909bf9dac8fde67f95db020ba7e8950f87be77ff
Mark Turner <turner.mark@abc.net.au>
2023-04-27 09:05:59 +1000

    Create `Package.swift` definitions.

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "lib-plugin-ios",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "YouboraLib",
+            targets: ["YouboraLib"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "YouboraLib",
+            path: "YouboraLib.xcframework"
+        ),
+        .testTarget(
+            name: "YouboraLibTests",
+            dependencies: ["YouboraLib"],
+            path: "YouboraLibTests"
+        )
+    ]
+)
+
