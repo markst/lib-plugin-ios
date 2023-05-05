ad5e51ee2194089d4d98e194b9d34806201466b2
Mark Turner <turner.mark@abc.net.au>
2023-05-05 16:14:07 +1000

    Update script to build tvOS slice.

--- scripts/build_framework.sh
+++ scripts/build_framework.sh
@@ -10,6 +10,10 @@ TMP_DIR="${BUILD_DIR}/Tmp"
 IOS_ARCHIVE_PATH="${TMP_DIR}/iOS.xcarchive"
 IOS_SIM_ARCHIVE_PATH="${TMP_DIR}/iOSSimulator.xcarchive"
 
+TVOS_SCHEME="YouboraLib tvOS"
+TVOS_ARCHIVE_PATH="${TMP_DIR}/tvOS.xcarchive"
+TVOS_SIM_ARCHIVE_PATH="${TMP_DIR}/tvOSSimulator.xcarchive"
+
 rm -rf ${BUILD_DIR}
 rm -rf "${FRAMEWORK_NAME}.xcframework"
 
@@ -27,9 +31,25 @@ xcodebuild archive \
     -sdk iphoneos \
     SKIP_INSTALL=NO
 
+xcodebuild archive \
+    -workspace "${WORKSPACE}" \
+    -scheme "${TVOS_SCHEME}" \
+    -archivePath ${TVOS_SIM_ARCHIVE_PATH} \
+    -sdk appletvsimulator \
+    SKIP_INSTALL=NO
+
+xcodebuild archive \
+    -workspace "${WORKSPACE}" \
+    -scheme "${TVOS_SCHEME}" \
+    -archivePath ${TVOS_ARCHIVE_PATH} \
+    -sdk appletvos \
+    SKIP_INSTALL=NO
+
 xcodebuild -create-xcframework \
     -framework ${IOS_SIM_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
     -framework ${IOS_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+    -framework ${TVOS_SIM_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+    -framework ${TVOS_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
     -output ${FRAMEWORK_NAME}.xcframework
 
 : '
